cefada732be5c776e8f9df7fb7d32bdc87d64d36
Alla_Kr <alla.kravchu@gmail.com>
2022-11-02 15:21:59 +0200

    edit code

--- my_project/my_game.py
+++ my_project/my_game.py
@@ -2,33 +2,29 @@
 import numpy as np
 def random_predict(number: int = 1) -> int:
     """Угадываем число
-
     Args:
         number (int, optional): _Загаданное число_. Defaults to 1.
-
     Returns:
         int: с какой попытки угадали
     """ 
-    np.random.seed(1)    
+    np.random.seed(1) # фиксируем загаданное число    
     number = np.random.randint(1, 101) # загадываем число
-    count = 0 # количество попыток
-    
-    print('Компьютер загадал число, Вам надо отгадать его за менее чем 20 попыток')
+    count = 0  # задаем счетчик
+    min_num = 1
+    max_num = 100
 
-    for i in range(1, 21):
-        choice = int(input("Введите число: "))
-        if choice > number:
-            print("Загаданное число меньше")
-        elif choice < number:
-            print("Загаданное число больше")
-        else:
-            print(f"Вы угадали число с {i} - ой попытки")
-            break
+    while True:
         count += 1
-    
-    if count >= 20:
-        print(f"Вы исчерпали все попытки, было загадано число {number}")
-    return(number) 
+        print('Число:', number, '---', 'Попытка:', count)
 
-print(f'Загаданное число отгадано: {random_predict()}')
-    
+        predict_num = int(( min_num + max_num) / 2)  # загадываем число как среднее между минимальным и максимальным и отбрасываем дробную часть
+
+        if predict_num > number:
+            max_num = predict_num
+        elif predict_num < number:
+            min_num = predict_num
+        else:
+            print(f'Алгоритм рассчитал число {number} за {count} попыток')
+            break
+    return count
+random_predict()
