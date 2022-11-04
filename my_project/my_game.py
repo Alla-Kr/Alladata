3ea9521dc721abb74722bfab7bcbe94fddc795a4
Alla_Kr <alla.kravchu@gmail.com>
2022-11-04 09:51:37 +0200

    edit code

--- my_project/my_game.py
+++ my_project/my_game.py
@@ -1,30 +1,55 @@
 """Игра угадай число"""
 import numpy as np
-def random_predict(number: int = 1) -> int:
-    """Угадываем число
+def random_predict(number: int = np.random.randint(1, 101)) -> int:
+   """Угадываем случайное число
+   Args:
+       number (int, optional): Загаданное число. Defaults to 1.
+   Returns:
+       int: Количество попыток
+   """
+ 
+   count = 0
+   max_number = 100
+   min_number = 0
+   #np.random.seed(1) # фиксируем для воспроизводимости 
+   predict_number = np.random.randint(1, 101) # загадываем число
+ 
+   while True:
+       count += 1
+ 
+       if predict_number > number:
+           max_number = predict_number - 1
+           predict_number = (max_number + min_number) // 2
+ 
+ 
+       elif predict_number < number:
+           min_number = predict_number + 1
+           predict_number = (max_number + min_number) // 2
+ 
+       else:
+           break # конец игры, выход из цикла
+ 
+   return count
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
     Args:
-        number (int, optional): _Загаданное число_. Defaults to 1.
+        random_predict ([type]): функция угадывания
+
     Returns:
-        int: с какой попытки угадали
-    """ 
-    np.random.seed(1) # фиксируем загаданное число    
-    number = np.random.randint(1, 101) # загадываем число
-    count = 0  # задаем счетчик
-    min_num = 1
-    max_num = 100
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+    while len(count_ls) < 1000:
 
-    while True:
-        count += 1
-        print('Число:', number, '---', 'Попытка:', count)
+        for number in random_array:
+            count_ls.append(random_predict(number))
 
-        predict_num = int(( min_num + max_num) / 2)  # загадываем число как среднее между минимальным и максимальным и отбрасываем дробную часть
-
-        if predict_num > number:
-            max_num = predict_num
-        elif predict_num < number:
-            min_num = predict_num
-        else:
-            print(f'Алгоритм рассчитал число {number} за {count} попыток')
-            break
-    return count
-random_predict()
+        score = int(np.mean(count_ls))
+        print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
